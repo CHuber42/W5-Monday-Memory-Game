1409e7e032d4c333937f982853b99e7edfaeb905
Christopher Huber <CHuber42.work@gmail.com>
2020-04-13 16:18:35 -0700

    add js to re-hide choices if pairs don't match

--- src/main.js
+++ src/main.js
@@ -5,13 +5,36 @@ import './styles.css';
 import { shuffle } from './shuffle.js';
 
 var cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+var targetFlag = 1;
+var target1 = 0;
+var target2 = 0;
 shuffle(cards);
+
 
 for (var i = 0; i < 12; i++){
   $(`#${i}`).append(cards[i]);
 }
 
-$("div").click(function(event) {
+$(".blue").click(function(event) {
   event.preventDefault();
-  $(this).children().show();
+  var card = $(this).children(); 
+  card.show();
+
+  console.log(card.text())
+  
+  if (targetFlag === 1) {
+    target1 = card;
+    targetFlag = 2;
+  }
+  else if (targetFlag === 2) {
+    target2 = card;
+    targetFlag = 1;
+  }
+  if (targetFlag === 1) {
+    if (target1.text() !== target2.text()) {
+      target1.hide();
+      target2.hide();
+    }
+  }
+
 });
